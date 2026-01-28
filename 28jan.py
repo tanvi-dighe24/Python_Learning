edaf6828b7cb73e131e9db8e97b8c197c4cc3a75
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-01-28 09:09:21 +0530

    Add files via upload
    Signed-off-by: Tanvee Dighe <tanvidighe1356@gmail.com>

---
+++ 28jan.py
@@ -0,0 +1,130 @@
+#if # 1. Age checking for vote
+if age >= 18
+    print("Eligible for vote")
+
+# 2. Positive number checking
+if num > 0:
+    print("Number is positive")
+
+# 3. Even number checkin
+if num % 2 == 0:
+    print("Even number")
+
+# 4. Marks checking
+if marks > 90:
+    print("Excellent")
+
+# 5. Leap year
+if year % 4 == 0:
+    print("Leap Year")
+
+# 6. Salary check
+if salary > 50000:
+    print("Tax applicable")
+
+# 7. Passwrd length check
+if len(password) > 8:
+    print("Password accepted")
+
+# 8. Temp alert
+if tempreture > 40:
+    print("High temperature alert")
+
+# 9. Stock check
+if quantity == 0:
+    print("Out of stock")
+
+# 10. Div by 3 and 5 (
+if num % 3 = 0 and num % = 0:
+    print("FizzBuzz")
+
+
+#if else ex-
+# 1. Even or Odd
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# 2. Vote eligibility
+if age >= 18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
+
+# 3. Positive or Neg
+if num >= 0:
+    print("positive")
+else:
+    print("negative")
+
+# 4. Pass or Fail
+if marks >= 35:
+    print("passed")
+else:
+    print("fdiled")
+
+# 5. Compare two numbers
+if a > b:
+    print("A is greater")
+else:
+    print("B is greater")
+
+# 6. Leap year or not
+if year % 4 == 0:
+    print("its leap year")
+else:
+    print("not a leap year")
+
+# 7. username question
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 8. Discount check
+if bill_amount > 2000:
+    print("discount applied")
+else:
+    print("cant give discount")
+
+# 9. uppercaselowercase
+if char.isupper():
+    print("Uppercase")
+else:
+    print("Lowercase")
+
+# 10. Divisibility by 7
+if num % 7 == 0:
+    print("Divisible by 7")
+else:
+    print("Not divisible by 7")
+
+
+
+
+
+
+
+
+
+
+
+t = (10, 11, 12, 13, 14, 15, 16)
+
+for i in range(len(t)):
+    if i % 2 != 0 and t[i] % 2 == 0:
+        print("found", t[i], "at index", i)  
+    
+
+    
