da47c86de2fff13e50d56081303795127ec19882
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-02-16 21:33:45 +0530

    Add files via upload
    Signed-off-by: Tanvee Dighe <tanvidighe1356@gmail.com>

--- Pattern_Coding_PYTHON.py
+++ Pattern_Coding_PYTHON.py
@@ -14,7 +14,7 @@ for i in range(1, n + 1):
     *     
   *       
 *         
-'''
+
 
 n = 5
 for i in range(1, n + 1):
@@ -24,8 +24,9 @@ for i in range(1, n + 1):
         else:
             print(' ', end=' ')
     print()
+    
 #o/p:
-'''
+
        * 
       * * 
     * * * 
@@ -77,12 +78,103 @@ i == 1 or i == n	Horizontal boundaries (Top & Bottom)
 j == 1 or j == n	Vertical boundaries (Left & Right)
 i == 3	                entire 3rd row (Horizontal bar)
 j == 3	                entire 3rd column (Vertical bar)
+
+
+'''   
+'''
+#----------------------------------------------------------------
+#o/p:1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5
+
+n=5
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(j, end=' ')
+    print()
+#--------------------------------------------------------------------
+#o/p:
+1 0 0 0 0 
+0 1 0 0 0 
+0 0 1 0 0 
+0 0 0 1 0 
+0 0 0 0 1 
+n=5
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j:
+            print('1', end=' ')
+        else:
+            print('0', end=' ')
+    print()
     
 
+#---------------------------------------------------------   
+
+#Pyramid Star Pattern
+n=5
+c= n*2-1
+for i in range(1,n+1):
+    for j in range(1, n+1):
+     if i+j > n+1:
+      print('*',end=' ')
+     else:
+      print(' ',end=' ')
+    print()  
+#O/P:
+        * 
+      * * 
+    * * * 
+  * * * * 
 
 
+n = 5
+for i in range(1, n + 1):
+    for j in range(1, 2 * n):
+        if i == n or i + j == n + 1 or j - i == n - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
 
 
+#O/P:
+    *    
+   * *   
+  *   *  
+ *     * 
+*********
+
+
+#Alphabetic Pattern
+for i in range(1, 5):
+    for j in range(i):
+        print(chr(65 + j), end="")
+    print()
+#Output:
+# A
+# AB
+# ABC
+# ABCD
+'''
+
+#Reversed No. Pattern
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j, end=' ')
+    print()
+
+#Output:
+#1 2 3 4 5 
+#1 2 3 4 
+#1 2 3 
+#1 2 
+#1
+
+    
+    
 
 
 
